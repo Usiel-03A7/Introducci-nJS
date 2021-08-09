3830bb0f8c8675055c2973d2319b4561153962ef
Usiel-03A7 <shebastyan@gmail.com>
2021-08-09 15:38:26 -0500

    learn objetc Math and funtions

---
+++ js/06.js
@@ -0,0 +1,14 @@
+// Objeto MATH 
+let resultado;
+
+resultado = Math.PI
+resultado = Math.round(2.5) 
+resultado = Math.ceil(2.9) // Ceil redondear hacia arriba 
+resultado = Math.floor(2.9) // Floor redondear hacia abajo 
+resultado = Math.sqrt(4) //squere root Raís cuadrada
+resultado = Math.abs(-200)
+resultado = Math.min(3,2,4,8,5,4,1)
+resultado = Math.max(3,2,4,8,5,4,1)
+resultado = Math.floor(Math.random() * 30)
+
+console.log(resultado);
