cb94c20ff8a356927247e8406ea4e1d4a0d7f416
Usiel-03A7 <shebastyan@gmail.com>
2021-08-09 14:43:39 -0500

    make operations with + - * / &

---
+++ js/05.js
@@ -0,0 +1,7 @@
+const num1 = 100
+const num2 = 200
+const nombre = 'Usiel'
+console.log(num1)
+console.log(num2)
+console.log(nombre)
+console.log('La suma es de: '+ (num1+num2)+' para nuestro apreciado '+nombre)
