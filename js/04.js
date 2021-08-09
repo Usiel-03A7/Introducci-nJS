033c0f0a2e22ab800c0d3ea9697c81493c91b8e6
Usiel-03A7 <shebastyan@gmail.com>
2021-08-09 14:26:11 -0500

    add the methot to String

---
+++ js/04.js
@@ -0,0 +1,15 @@
+const tweet = 'al compita usiel le gusta programar ' //String o cadenas de texto
+const producto2 = 'Monitor HD '
+
+ //lenght es para saber el numero de caracter
+console.log(tweet.length)
+console.log(producto2.length)
+
+//IndexOf para saber si existe en la cadena la palabra 
+console.log(tweet.indexOf('usiel'))
+console.log(producto2.indexOf("tablet"))
+
+//Include
+
+console.log(tweet.includes('usiel'))
+console.log(producto2.includes("tablet"))
