653161368cae5bcec9359a511ebc153f46ded11c
Usiel-03A7 <shebastyan@gmail.com>
2021-08-09 10:53:56 -0500

    start variable let and const

---
+++ js/02.js
@@ -0,0 +1,25 @@
+//Variables con let
+// Variables
+
+let producto = 'Mause Gamer' //Iniciar Variable y asignar un valor
+
+let disponible  //Iniciamos una variable para despues asignar un valor
+
+// producto = true; // reasignando un valor a la variable
+ 
+disponible=true;
+
+
+let producto1 = 'computadora',
+    disponible=true,
+    departamento='computadoras';
+
+
+//Variables 
+let disponible_;
+
+//Estilos para variables
+let nombre_producto = 'Monitor HD' //underscore
+let nombreProducto = 'Monitor HD' // Camelcase
+let NombrePrducto = 'Monitor HD' // pascalcase
+
---
+++ js/03.js
@@ -0,0 +1,25 @@
+// Variables
+
+const producto = 'Mause Gamer' //Iniciar Variable y asignar un valor
+
+
+
+// producto = true; // reasignando un valor a la variable
+ 
+
+
+
+const producto1 = 'computadora',
+    disponible=true,
+    departamento='computadoras';
+
+
+//Variables 
+
+
+//Estilos para variables
+const nombre_producto = 'Monitor HD' //underscore
+const nombreProducto = 'Monitor HD' // Camelcase
+const NombrePrducto = 'Monitor HD' // pascalcase
+
+console.log(producto)
