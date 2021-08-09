d04350f6087ecefd8e6f8d62c7ea4e6b32aeecaa
Usiel-03A7 <shebastyan@gmail.com>
2021-08-09 10:43:24 -0500

    start JS with vartiables Var

---
+++ js/01.js
@@ -0,0 +1,25 @@
+// Variables
+
+var producto = 'Mause Gamer' //Iniciar Variable y asignar un valor
+
+var disponible  //Iniciamos una variable para despues asignar un valor
+
+// producto = true; // reasignando un valor a la variable
+ 
+disponible=true;
+
+
+var producto1 = 'computadora',
+    disponible=true,
+    departamento='computadoras';
+
+
+//Variables 
+var disponible_;
+
+//Estilos para variables
+var nombre_producto = 'Monitor HD' //underscore
+var nombreProducto = 'Monitor HD' // Camelcase
+var NombrePrducto = 'Monitor HD' // pascalcase
+
+console.log(producto)
