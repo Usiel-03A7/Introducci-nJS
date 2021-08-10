a85694a454a5bee3ae7c4c0e1beffd1991be6347
Usiel-03A7 <shebastyan@gmail.com>
2021-08-10 11:10:46 -0500

    print the increase

---
+++ js/07.js
@@ -0,0 +1,15 @@
+// Orden de las operaciones 
+let resultado; 
+resultado = 20 + 30 * 2;
+
+resultado = (600 +600) * 1.05; //para saber cuanto se le aumenta por impuesto
+
+
+// console.log(resultado);
+
+// Incrementos
+let puntaje = 10;
+
+puntaje++
+
+console.log(puntaje)
