0d2f398d154585770e3875e9a2b3adf0268c9aba
Usiel-03A7 <shebastyan@gmail.com>
2021-08-10 13:52:21 -0500

    add or delet  atributes to objets

--- js/10.js
+++ js/10.js
@@ -1,12 +1,19 @@
-// Objetos 
-const nombreProducto = ' Monitor 20" ';
-const precio = 300;
-const disponible = true;
-
+// // Objetos 
+ 
 const producto = {NombreProducto : 'Monitor de 20" ',
         precio: 300,
         disponible: true
 }
+//Para agregar nuevas propiedades 
+producto.imagen = "imagen.jpg"
+
+// Para eliminar propiedades
+
+delete producto.disponible  
+
+//imprimir
 console.log(producto)
-console.log("$"+producto.precio)
-console.log(producto["precio"])
+
+// console.log(producto)
+// console.log("$"+producto.precio)
+// console.log(producto["precio"])
