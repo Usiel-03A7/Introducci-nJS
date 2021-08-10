e02c9a0203d1f5c3804bbcb948bca3130dd3a92a
Usiel-03A7 <shebastyan@gmail.com>
2021-08-10 12:28:46 -0500

    make objets add properties

---
+++ js/10.js
@@ -0,0 +1,12 @@
+// Objetos 
+const nombreProducto = ' Monitor 20" ';
+const precio = 300;
+const disponible = true;
+
+const producto = {NombreProducto : 'Monitor de 20" ',
+        precio: 300,
+        disponible: true
+}
+console.log(producto)
+console.log("$"+producto.precio)
+console.log(producto["precio"])
