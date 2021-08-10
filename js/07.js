87532cbaf553e18c35c4322892a38090bd2d1af5
Usiel-03A7 <shebastyan@gmail.com>
2021-08-10 11:58:56 -0500

    make Template String js

--- js/07.js
+++ js/07.js
@@ -10,6 +10,7 @@ resultado = (600 +600) * 1.05; //para saber cuanto se le aumenta por impuesto
 // Incrementos
 let puntaje = 10;
 
-puntaje++
+puntaje++; //imprime 10 y luego aumenta
+++puntaje; //aumenta primero y despues imprime 12 por el código anterior que aumenta +1 y el inicio +1
 
 console.log(puntaje)
---
+++ js/08.js
@@ -0,0 +1,8 @@
+const nombre = 'Usiel Solano'
+const correo = 'correo@ejemplo.com'
+
+// Concatenacion
+console.log(nombre +" "+ correo)
+
+// Template String
+console.log(`Nombre Cliente: ${nombre} Email: ${correo}`)
