92f0419c086e86bea47ed32a5038b47e136478e3
Usiel-03A7 <shebastyan@gmail.com>
2021-08-11 14:01:44 -0500

    make array JS

---
+++ js/14.js
@@ -0,0 +1,17 @@
+// Arreglos 
+
+const numeros = [10,20,30,40,50]
+
+console.table(numeros)
+
+const meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo']
+
+console.table(meses)
+
+//acceder a un dato de un arreglo
+
+console.log(meses[3])
+
+//conocer el total de un arreglo
+
+console.log(meses.length)
