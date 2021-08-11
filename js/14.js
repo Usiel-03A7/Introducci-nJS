fdd020a3994c266eb469428df3a3adfb45d725d7
Usiel-03A7 <shebastyan@gmail.com>
2021-08-11 15:43:45 -0500

    learn array and method push y pop

--- js/14.js
+++ js/14.js
@@ -6,12 +6,30 @@ console.table(numeros)
 
 const meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo']
 
-console.table(meses)
+// console.table(meses)
 
-//acceder a un dato de un arreglo
+// acceder a un dato de un arreglo
 
-console.log(meses[3])
+
 
 //conocer el total de un arreglo
 
-console.log(meses.length)
+// console.log(meses.length)
+numeros.push(1654)  // Agrega al final  numeros
+numeros.unshift(5)  //agrega al inicio
+
+
+// meses.pop() //quita en cola (al fina)
+// meses.shift() //quita  al principio 
+
+
+    // meses.s plice(1,1)
+console.table(numeros)
+console.table(meses)
+
+//rest operatior or spread operator
+
+const nuevomeses = [...meses, 'junio'] //así simulas el push
+const nuevomeses = ['junio', ...meses] //así simulas el unshift
+
+console.table(nuevomeses)
