7666da56c84c7c8655ff4d24d9b97558021658e3
Usiel-03A7 <shebastyan@gmail.com>
2021-08-11 21:19:56 -0500

    Learn diference with method and function

---
+++ js/17.js
@@ -0,0 +1,8 @@
+// diferencia entre método y funcion 
+const num1 = 20;
+const num2 = '20';
+const name = 'Usiel'
+console.log(num2);
+console.log(name);
+console.log( parseInt(num2) ); // ParseInt es una funcion 
+console.log(num1.toString()); // .toString es un método (el que tiene punto es método)
