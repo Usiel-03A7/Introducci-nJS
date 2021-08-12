7f8731e82b91275020c7a59f33dcdb5acd714664
Usiel-03A7 <shebastyan@gmail.com>
2021-08-12 13:20:08 -0500

    Learn if else if else JS

---
+++ js/22.js
@@ -0,0 +1,24 @@
+// Estructuras de control
+// const puntaje = 1000;
+
+// if(puntaje === 1000){
+//     console.log(`cumple con la condicion`)
+// }else {
+//     console.log(`No es igual`)
+// }
+// const efectivo = 800;
+// const carrito = 800;
+// if (efectivo >= carrito) {
+//     console.log('Articulo comprado')
+// } else {
+//     console.log("no te ajusta may, ya fue")
+// }
+const rol = 'Administrador'
+if (rol=== 'Administrador') {
+    console.log("acceso correcto")
+} else if ( rol=== 'editor'){
+    console.log("acceso de editor")
+} 
+else {
+    console.log("acceso incorrecto")
+}
