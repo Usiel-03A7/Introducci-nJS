1c36d43f7f6b4f38ff8c36a6ee86ad2c725e5745
Usiel-03A7 <shebastyan@gmail.com>
2021-08-11 21:02:47 -0500

    Learn functions declaration

---
+++ js/16.js
@@ -0,0 +1,21 @@
+// Declaracion de la funcion
+function sumar(){
+ console.log(10+10)
+    
+}
+sumar()
+
+//expersion de la funcion 
+
+const suma2 = function(){
+    console.log(3.4+63)
+}
+
+suma2();
+
+//IIFE
+
+(function() { 
+    console.log('Ésto es una funcion');
+})();
+console.log(cliente)
---
+++ js/otro.js
@@ -0,0 +1,4 @@
+//IIFE es para protejer y que no se mezclen las variables con otros archivos
+(function() { 
+    const cliente = 'juan'    
+})();
