3c608f79484cd1cf107869d1a8948454734d998a
Usiel-03A7 <shebastyan@gmail.com>
2021-08-12 16:12:17 -0500

    learn foreach and map

---
+++ js/25.js
@@ -0,0 +1,39 @@
+
+const carrito = [
+    {
+        nombre: 'monitor 20"',
+        precio: 2500
+    }, {
+        nombre: 'Teclado',
+        precio: 300
+    }, {
+        nombre: 'mause',
+        precio: 250
+    }, {
+        nombre: 'hub usb',
+        precio: 100
+    }, {
+        nombre: 'audifonos',
+        precio: 600
+    }, {
+        nombre: 'silla gamer',
+        precio: 5000
+    }, {
+        nombre: 'tapete gamer',
+        precio: 300
+    }
+]
+// for each imprimir en consola el foreach
+
+carrito.forEach( producto => console.log(producto.nombre));
+const arreglo1 = carrito.forEach( producto => console.log(producto.nombre));
+
+
+//map  crear un nuevo arreglo con caracteristicas usar
+
+carrito.map( producto => console.log(producto.nombre));
+const arreglo2 = carrito.map( producto => console.log(producto.nombre));
+console.log("arreglo1");
+console.log(arreglo1);
+console.log("arreglo2");
+console.log(arreglo2);
