48bbc8246fe57c99d1065cd9547ebb0512901d67
Usiel-03A7 <shebastyan@gmail.com>
2021-08-12 16:13:32 -0500

    Foreach map

--- js/25.js
+++ js/25.js
@@ -25,14 +25,14 @@ const carrito = [
 ]
 // for each imprimir en consola el foreach
 
-carrito.forEach( producto => console.log(producto.nombre));
+carrito.forEach( producto => console.log(producto.nombre)); // así se imprime 
 const arreglo1 = carrito.forEach( producto => console.log(producto.nombre));
 
 
 //map  crear un nuevo arreglo con caracteristicas usar
 
-carrito.map( producto => console.log(producto.nombre));
-const arreglo2 = carrito.map( producto => console.log(producto.nombre));
+carrito.map( producto => console.log(producto.nombre)); //así se imprime 
+const arreglo2 = carrito.map( producto => console.log(producto.nombre)); //así se copia el arreglo
 console.log("arreglo1");
 console.log(arreglo1);
 console.log("arreglo2");
