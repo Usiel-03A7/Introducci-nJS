1ac14a152aba3a1f37d1d7ca03f54f285eb81de2
Usiel-03A7 <shebastyan@gmail.com>
2021-08-13 17:30:54 -0500

    Learn Prototype JS

--- js/27.js
+++ js/27.js
@@ -1,22 +1,44 @@
 // Poo 
 
 // Object Literal
+
 const producto = {    
     nombre: "table",
     precio: 2540,
     
 }
+function Cliente(nom,apelli) {
+    this.nom = nom;
+    this.apelli = apelli;
+    
+}
 
 // Object Constructor 
 
-function Producto(nom,prec, marca) {
+function Producto(nom,prec) {
     this.nom = nom;
     this.prec = prec;
-    this.marca = marca;
+    
 }
-const producto2 = new Producto(`Monitor curvo de 49`, 5000,"Philips");
-const producto3 = new Producto(`celular note 20 ultra`, 30000,"Samsung");
-const producto4 = new Producto(`laptop gamer`, 25000,"Dell");
-console.log(producto2);
-console.log(producto3);
-console.log(producto4);
+// Crear funciones  que solo se utilizan en  un objeto en específico. Prototype
+Producto.prototype.formatearP = function(){
+    return `${this.nom} tiene el precio de $${this.prec}`
+}
+
+const producto2 = new Producto(`Monitor curvo de 49`, 5000);
+const producto3 = new Producto(`celular note 20 ultra`, 30000);
+const cliente = new Cliente("Pedro que gusto de verte", "supe que eras licenciado")
+
+
+
+function formatearP (producto){
+    return `${producto.nom} tiene el precio de $${producto.prec}`
+}
+
+console.log(producto2)
+console.log(producto2.formatearP());
+console.log(producto3.formatearP());
+
+
+
+// Crear funciones que solo se usan en un objeto en especifico 
