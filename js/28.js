d2000e2fa27bbc3e791937104e79f17a8acc4244
Usiel-03A7 <shebastyan@gmail.com>
2021-08-13 18:03:40 -0500

    learn extends and super with JS

--- js/28.js
+++ js/28.js
@@ -9,10 +9,31 @@ class Producto {
         return `${this.nombre} tiene el precio de $${this.precio}`
     }
     regresaPrecio(){
-        return `el precio del producto es ${this.precio}`
+        console.log(this.precio)
     }
 }
 const producto2 = new Producto(`Monitor curvo de 49`, 5000);
 const producto3 = new Producto(`celular note 20 ultra`, 30000);
-console.log(producto2);
-console.log(producto3);
+
+
+// Herencia 
+
+class Libros extends Producto{
+    constructor(nombre, precio, isbn){
+        super(nombre, precio);
+        this.isbn = isbn;
+    }
+    formatearProduct(){
+        return `${super.formatearProduct()} y su ISBN es: ${this.isbn}`
+    }
+    obtenerPrecio(){
+        super.regresaPrecio();
+    }
+}
+const libro = new Libros("Javascript la revolucion", 120, "658549889564");
+
+console.log(libro.formatearProduct()); 
+console.log(libro.obtenerPrecio()); 
+
+console.log(producto2.formatearProduct());
+console.log(producto2.regresaPrecio());
