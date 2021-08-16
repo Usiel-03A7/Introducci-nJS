a70fec6d216d5a0becf8b5ce4298d307abd7778b
Usiel-03A7 <shebastyan@gmail.com>
2021-08-16 12:51:21 -0500

    learn array promises and async await

--- js/32.js
+++ js/32.js
@@ -9,14 +9,27 @@ function descargarNC() {
         }, 1000);
     })
 }
+function descargarUltimos() {
+    return new Promise(resolve => {
+        console.log('descargando pedidos espere...')
+
+        setTimeout(() => {
+            resolve('los pedidos fueron descargados')
+        }, 1000);
+    })
+}
 async function app() {
     try {
-        const resultado = await descargarNC();
-        console.log(resultado)
+        // const clientes = await descargarNC();
+        // const pedidos = await descargarUltimos(); 
+        // console.log(clientes)
+        // console.log(pedidos)
+        const resultado = await Promise.all([  descargarNC(), descargarUltimos() ]); //arreglo de promises Promise.all
+        console.log(resultado[0])
+        console.log(resultado[1])
     } catch (error) {
         console.log(error)
     }
 }
 
 app();
-console.log('éste codigo no se bloquea')
